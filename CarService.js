6a21b4dec1515dcebb1a9e58bb0fb17ab55bd975
Aazam <abuzarali1098@gmail.com>
2024-10-08 19:23:52 +0530

    Fixed the navbar on top when scrolling

---
+++ CarService.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+    if (window.scrollY >= sticky) {
+        // navbar.classList.add("fixed");
+        navbar.style.top = '0';
+    } else {
+        // navbar.classList.remove("fixed");
+        navbar.style.top = '6vh';
+    }
+}

49e8dbeed19eefaf917e150478a3874bfa854a7b
Aazam <abuzarali1098@gmail.com>
2024-10-12 09:06:18 +0530

    added swiper slider instead of custom slider

---
+++ js files/swiper.js
@@ -0,0 +1,27 @@
+new Swiper('.what-clients-say', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breapoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+    }
+  });
